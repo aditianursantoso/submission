4d00e61479c70d47303fa6ef15647fd56042a388
aditianursantoso <aditia.nursantoso@kbi.krakatausteel.com>
2023-08-14 16:31:40 +0700

    status model, db factories, db seeders

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Status extends Model
 {
     use HasFactory;
+
+    // class constants
+    public const PENDING = ['id' => 1, 'name' => 'Pending'];
+    public const APPROVED = ['id' => 2, 'name' => 'Approved'];
+    public const REJECTED = ['id' => 3, 'name' => 'Rejected'];
 }
