d84cbc5afe66e4c872c53868bc9fbdfd7d9a965a
aditianursantoso <aditia.nursantoso@kbi.krakatausteel.com>
2023-08-15 10:29:45 +0700

    history model, factory, seeder

--- database/factories/SubmissionFactory.php
+++ database/factories/SubmissionFactory.php
@@ -12,36 +12,33 @@ use Illuminate\Support\Str;
  */
 class SubmissionFactory extends Factory
 {
-  /**
-   * Define the model's default state.
-   *
-   * @return array<string, mixed>
-   */
-  public function definition(): array
-  {
-    return [
-      'user_id' => User::factory(),
-      'status_id' => Status::factory(),
-      'type' => fake()->randomElement(['Leave', 'Permit', 'Sick']),
-      'attachment' => 'attachments/' . Str::random(40) . '.jpg',
-    ];
-  }
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'status_id' => Status::factory(),
+            'type' => fake()->randomElement(['Leave', 'Permit', 'Sick']),
+            'attachment' => 'attachments/' . Str::random(40) . '.jpg',
+        ];
+    }
 
-  public function pending(): static
-  {
-    return $this->state(fn (array $attributes) =>
-    ['status_id' => Status::PENDING['id']]);
-  }
+    public function pending(): static
+    {
+        return $this->state(fn(array $attributes) => ['status_id' => Status::PENDING['id']]);
+    }
 
-  public function approved(): static
-  {
-    return $this->state(fn (array $attributes) =>
-    ['status_id' => Status::APPROVED['id']]);
-  }
+    public function approved(): static
+    {
+        return $this->state(fn(array $attributes) => ['status_id' => Status::APPROVED['id']]);
+    }
 
-  public function rejected(): static
-  {
-    return $this->state(fn (array $attributes) =>
-    ['status_id' => Status::REJECTED['id']]);
-  }
+    public function rejected(): static
+    {
+        return $this->state(fn(array $attributes) => ['status_id' => Status::REJECTED['id']]);
+    }
 }
